070f38c2c8a08e9b766f884fc57d19cdafb78993
YubiSora <94666378+YubiSora@users.noreply.github.com>
2021-12-02 12:25:14 +0700

    Update alpha.js

--- alpha.js
+++ alpha.js
@@ -2747,7 +2747,7 @@ for (let i of ownerNumber) {
 const vname = alpha.contacts[i] != undefined ? alpha.contacts[i].vname || alpha.contacts[i].notify : undefined
 ini_list.push({
 "displayName": `Developer HoshiBot`,
-"vcard":, `BEGIN:VCARD\nVERSION:3.0\nN:Sy;${NamaOwner};;;\nFN:${vname ? `${vname}` : `${NamaOwner}`}\nitem1.TEL;waid=+6289674545497:+62895613027623\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;${NamaOwner};;;\nFN:${vname ? `${vname}` : `${NamaOwner}`}\nitem1.TEL;waid=+6289674545497:+62895613027623\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
 "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;${NamaOwner};;;\nFN:${vname ? `${vname}` : `${NamaOwner}`}\nitem1.TEL;waid=+6289674545497:+62895613027623\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
 })
 }
